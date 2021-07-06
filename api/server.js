9a0a75ce45c2edc1dd26950740e7e729a4f63c38
Devon Osei <oseidevon@gmail.com>
2021-07-06 02:21:14 -0400

    ss

--- api/server.js
+++ api/server.js
@@ -7,6 +7,18 @@ const server = express()
 
 server.use(express.json())
 
+server.delete('/api/users/:id', async (req, res) => {
+  const possibleUser = await User.findById(req.params.id)
+  if (!possibleUser) {
+    res.status(404).json({
+      message:'The user with the specified ID does not exist',
+    })
+  } else {
+    const deletedUser = await User.remove(possibleUser.id)
+    res.status(200).json(deletedUser)
+  }
+})
+
 server.post('/api/users', (req, res) => {
   const user = req.body;
   if (!user.name || !user.bio) {
@@ -31,7 +43,7 @@ server.post('/api/users', (req, res) => {
 server.get( '/api/users', (req, res) => {
   User.find()
   .then(user => {
-    res.json(users)
+    res.json(user)
   })
   .catch(err => {
     res.status(500).json({
@@ -47,7 +59,7 @@ server.get( '/api/users/:id', (req, res) => {
   .then(user => {
     if (!user) {
     res.status(404).json({
-      message:"The user with the specified ID doesnot exist",
+      message:"The user with the specified ID does not exist",
      })
     }
     res.json(user)
